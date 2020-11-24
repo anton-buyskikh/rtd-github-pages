a51fb3d328596eafb44bc3c4584e213b9cf9cf60
Anton Buyskikh <anton.buyskikh@gmail.com>
2020-11-24 21:25:00 +0000

    Update version

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -26,7 +26,7 @@ pip install --no-cache-dir \
 
 # list which branches and tags will be build
 # TODO: when the release process is settled this will need to be automated
-DOCSVERSIONS="dev v0.1.0 v0.1.1"
+DOCSVERSIONS="dev v0.1.1 v0.2.0"
 # TODO: this variable is defined in docs/Makefile as well
 BUILDDIR="_build"
 
